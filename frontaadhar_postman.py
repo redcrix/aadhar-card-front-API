cf299b6b283ea11b161bd079bd5beaeba8e425ac
redcrix <contact@redcrix.com>
2019-11-19 17:31:39 +0530

    Add files via upload

--- frontaadhar_postman.py
+++ frontaadhar_postman.py
@@ -8,30 +8,34 @@ import cv2
 import numpy as np
 import re
 import face_recognition
+import time
 
 # For error use:- pytesseract.pytesseract.TesseractNotFoundError: tesseract is not installed or it's not in your
-#
+
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/aad_front_side', methods=['POST', 'GET'])
 def front_name_dob_number():
+
+    time_str = time.strftime("%Y%m%d-%H%M%S")
+
     if request.method == 'POST':
         print(request.files)
-        if 'id1' not in request.files:
+        if 'idfront' not in request.files:
             return json.dumps({"message": 'No image of frontside of aadhar card', "success": False, "data": ''})
-        id1 = request.files['id1']
-        id1.save("front1.jpg")
-        tup = cv2.imread("front1.jpg")
+        idfront = request.files['idfront']
+        idfront.save(time_str + '_front1.jpg')
+        tup = cv2.imread(time_str + '_front1.jpg')
 
         if request.method == 'POST':
             print(request.files)
-            if 's1' not in request.files:
+            if 'p_image' not in request.files:
                 return json.dumps({"message": 'No selfie image of user', "success": False, "data": ''})
-            s1 = request.files['s1']
-            s1.save("selfie.jpg")
-            tup2 = cv2.imread("selfie.jpg")
+            p_image = request.files['p_image']
+            p_image.save(time_str + '_p_image.jpg')
+            tup2 = cv2.imread(time_str + "_p_image.jpg")
 
         gray = cv2.cvtColor(tup, cv2.COLOR_BGR2GRAY)
         face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
@@ -41,13 +45,12 @@ def front_name_dob_number():
             ix = 0
             cv2.rectangle(tup, (x, y), (x + w, y + h), (0, 255, 0), 2)
             roi_color = tup[y:y + h, x:x + w]
-            str1 = str(w) + str(h) + str(x) + str(ix)
-            #str2 = str(ix) + str(y) + str(x) + str(w)
-            crop_pic = cv2.imwrite(str1 + '.jpg', roi_color)
+
+            crop_pic = cv2.imwrite(time_str + '_croppic.jpg', roi_color)
             ix = ix + 1
 
-        known_image = face_recognition.load_image_file('selfie.jpg')
-        unknown_image = face_recognition.load_image_file(str1 + '.jpg')
+        known_image = face_recognition.load_image_file(time_str + '_p_image.jpg')
+        unknown_image = face_recognition.load_image_file(time_str + '_croppic.jpg')
         face_locations = face_recognition.face_locations(unknown_image)
         face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
         bide_encoding = face_recognition.face_encodings(known_image)[0]
